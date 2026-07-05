a77f907b61a35e4ed98525ad2fefa8293bb66b18
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-07-05 14:51:40 +0530

    commited new prog like ceiling and floor

---
+++ BinarySearch/Celing.java
@@ -0,0 +1,43 @@
+package BinarySearch;
+
+public class Celing {
+
+    public static void main(String []args){
+		int arr[]={1,2,4,5,6,7,8,9};
+		int target=3;
+		int ans=celing(arr,target);
+		System.out.println(ans);
+		}
+
+	
+
+	//return index if it exist
+	//return -1 if it does not exist
+	static int celing(int arr[],int target){
+		int start = 0;
+		int end =arr.length-1;
+		while(start<=end){
+			//finding middle element
+			int mid=(start+end)/2;
+			if(target<arr[mid]){
+				end=mid-1;
+			}
+			else if(target>arr[mid]){
+				start=mid+1;
+
+			}
+			else{
+				return mid;
+			}
+		
+		
+		}
+		return start;
+	
+
+}
+
+
+
+    
+}
---
+++ BinarySearch/Floor.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+public class Floor {
+
+    public static void main(String []args){
+        int arr[]={1,2,4,5,6,7,8,9};
+        int target=3;
+        int ans=floor(arr,target);
+        System.out.println(ans);
+        }
+
+    
+
+    //return index if it exist
+    //return -1 if it does not exist
+    static int floor(int arr[],int target){
+        int start = 0;
+        int end =arr.length-1;
+        while(start<=end){
+            //finding middle element
+            int mid=(start+end)/2;
+            if(target<arr[mid]){
+                end=mid-1;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+
+            }
+            else{
+                return mid;
+            }
+        
+        
+        }
+        return end;
+    
+}
+}
