f52378bcbf98ef36e18f8cb80cd6fc029544f6e4
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-07-09 15:04:57 +0530

    commited new prog like ceiling and floor

--- BinarySearch/BinarySearch1.java
+++ BinarySearch/BinarySearch1.java
@@ -17,7 +17,7 @@ public class BinarySearch1 {
 		int end =arr.length-1;
 		while(start<=end){
 			//finding middle element
-			int mid=(start+end)/2;
+			int mid=start+(end-start)/2;
 			if(target<arr[mid]){
 				end=mid-1;
 			}
---
+++ TwoDarrayBS/SortedTwoDarraySearch.java
@@ -0,0 +1,34 @@
+public class SortedTwoDarraySearch {
+    
+    public static void main(String []args){
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int target=5;
+        int ans=search(arr,target);
+        System.out.println(ans);
+        }
+
+    
+
+    //return index if it exist
+    //return -1 if it does not exist
+    static int search(int arr[][],int target){
+
+        int row=0;
+        int col=arr.length-1;
+
+        while(row<arr.length && col>=0){
+            if(arr[row][col]==target){
+                System.out.println("row:"+row+" col:"+col);
+                return 1;
+            }
+            else if(arr[row][col]<target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+        return -1;
+    }               
+    
+}
