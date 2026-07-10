cb76bc375e41ba00d20bf27f2f7821c7ae2f925e
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-07-10 10:52:08 +0530

    commited selection sort

---
+++ TwoDarrayBS/sorts/SelectionSort.java
@@ -0,0 +1,40 @@
+package sorts;
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[]args){
+        int[]arr={2,5,6,1,3,4};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+
+    static void selection(int[]arr){
+        for(int i=0;i<arr.length;i++){
+            int last=arr.length-i-1;
+            int maxIndex=getMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+
+    public static void swap(int[]arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
+
+
+
+
+    public static int getMaxIndex(int[]arr,int start,int end){
+        int max=start;
+        for(int i=start;i<=end;i++){
+            if(arr[i]>arr[max]){
+                max=i;
+            }
+        }
+        return max;
+    }
+}
